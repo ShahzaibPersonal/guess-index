786d30f7889b8ba5a16e1442493ec8a0cf65bc95
ShahzaibPersonal <52831453+ShahzaibPersonal@users.noreply.github.com>
2022-05-26 03:30:55 +0500

    Final Push With Both Solution Limit is 0 to 17576 for my original solution. But other one is not limited to anything.

--- index.js
+++ index.js
@@ -4,11 +4,51 @@ const guessBtn = document.getElementById("guessBtn")
 let alpArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 let answer = []
 
+
+let columnName = [];
+
+
+// used recursion rest is same as GEEK FOR GEEKS SOLUTION 
+// https://www.geeksforgeeks.org/find-excel-column-name-given-number/
+// this solution is proper not mine. I dont own this one but other one is mine.
+
+function printString(columnNumber) {
+
+    if (columnNumber > 0) {
+
+        let rem = columnNumber % 26;
+
+        if (rem == 0) {
+            columnName.push("Z");
+            columnNumber = Math.floor(columnNumber / 26) - 1;
+        }
+        else // If remainder is non-zero
+        {
+            columnName.push(String.fromCharCode((rem - 1) + 'A'.charCodeAt(0)));
+            columnNumber = Math.floor(columnNumber / 26);
+        }
+
+        printString(columnNumber)
+
+    }
+    else {
+        columnName.reverse().join("")
+        
+    }
+
+}
+
+
 guessBtn.addEventListener('click', () => {
 
+    printString(number.value)
+    console.log(columnName)
+
+
     answer = []
-    if (number.value == "") {
-        alert("Please enter some number")
+    
+    if (number.value == "" || number.value > 17576) {
+        alert("Please Enter a value which range between 0 and 17576. Thanks !!")
     }
     else {
 
@@ -47,33 +87,58 @@ guessBtn.addEventListener('click', () => {
             alert("Index for number " + number.value + " will be " + answer[0] + answer[1])
 
         }
-        else if ((quotient >= 27 && quotient < (1 + 26 + 676)) || (quotient >= (1 + 26) && quotient == (1 + 26 + 676) && remainder == 0)) {
+        else if ((quotient => 27 && quotient < (703)) || (quotient >= 27 && quotient == 703 && remainder == 0)) {
 
             console.log("level 3")
 
-            let ans = number.value / 26;
-            // console.log(quotient + " " + remainder + " " )
-
-
-            ans = ans - quotient
-            console.log("ans with quote subtract" + ans)
-
-            let temp = ans * 26
-
-            console.log("temp value " + Math.round(temp))
+            // what ever we do answer will be in this range 
             answer = ["A", "A", "A"]
 
-            answer[2] = alpArray[Math.round(temp) - 1]
 
-            if (quotient - 27 > 27) {
 
-                ans = quotient / 26
-                console.log("ans again " + ans)
 
+            let ans = number.value / 26;
+
+            ans = ans - quotient
+            let temp = ans * 26
+
+            console.log("temp value was" + temp)
+
+
+
+            if (temp == 0) {
+                answer[2] = "Z"
+            }
+            else {
+
+                answer[2] = alpArray[Math.round(temp) - 1]
+            }
+
+            let num = number.value
+
+
+            while (quotient >= 27) {
+
+                remainder = num % 26
+                quotient = ~~(num / 26)
+                console.log(quotient + " " + remainder + " " + num / 26)
+                answer[0] = alpArray[quotient - 1]
+
+                if (remainder == 0) {
+                    answer[1] = "Z"
+                }
+                else {
+                    answer[1] = alpArray[remainder - 1]
+                }
+                num = quotient;
 
             }
 
+
+
+
             alert("Index for number " + number.value + " will be " + answer)
+
 
         }
 
@@ -84,13 +149,11 @@ guessBtn.addEventListener('click', () => {
         // })
 
 
-        while (number.value != 0) {
-            remainder = number.value % 26
-
-        }
-        number.value
 
     }
 
 
 })
+
+
+
